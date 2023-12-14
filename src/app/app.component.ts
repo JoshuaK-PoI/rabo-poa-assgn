181dba925e905309da2bbb4e11bfa6d749d9559c
Joshua Kobes <joshua.kobes@hotmail.com>
2023-12-14 14:45:59 +0100

    Remove unneeded test and provide data source for dev

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { CUSTOMER_DATA_SOURCE_TOKEN } from '../datasource/customer-data-source';
+import { DevCustomerDataSource } from '../datasource/dev-customer-data-source';
+import { CustomerService } from './customer.service';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +15,10 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     NavbarComponent,
     RouterLink,
     RouterLinkActive,
+  ],
+  providers: [
+    { provide: CUSTOMER_DATA_SOURCE_TOKEN, useClass: DevCustomerDataSource },
+    CustomerService,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
