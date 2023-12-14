15d928aad47497671188fe3c3fd5026aec4c3082
Joshua Kobes <joshua.kobes@hotmail.com>
2023-12-14 15:33:10 +0100

    Remove unused modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CUSTOMER_DATA_SOURCE_TOKEN } from '../datasource/customer-data-source';
 import { DevCustomerDataSource } from '../datasource/dev-customer-data-source';
@@ -9,13 +9,7 @@ import { CustomerService } from './customer.service';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    NavbarComponent,
-    RouterLink,
-    RouterLinkActive,
-  ],
+  imports: [CommonModule, RouterOutlet, NavbarComponent],
   providers: [
     { provide: CUSTOMER_DATA_SOURCE_TOKEN, useClass: DevCustomerDataSource },
     CustomerService,
