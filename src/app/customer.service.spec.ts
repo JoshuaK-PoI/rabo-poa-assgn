b86ac8ef99d473ca5aac7f1e61767fd0089cdfe8
Joshua Kobes <joshua.kobes@hotmail.com>
2023-12-14 14:47:42 +0100

    Add tests for customer data service

--- src/app/customer.service.spec.ts
+++ src/app/customer.service.spec.ts
@@ -1,16 +1,59 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CustomerService } from './customer.service';
+import { CUSTOMER_DATA_SOURCE_TOKEN } from '../datasource/customer-data-source';
+import {
+  TestCustomerDataSource,
+  customerServiceMockResponse,
+} from '../datasource/test-customer-data-source';
 
 describe('CustomerService', () => {
   let service: CustomerService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: CUSTOMER_DATA_SOURCE_TOKEN,
+          useClass: TestCustomerDataSource,
+        },
+        CustomerService,
+      ],
+    });
     service = TestBed.inject(CustomerService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return all customers', waitForAsync(() => {
+    service.getCustomers().subscribe((response) => {
+      expect(response.customers).toEqual(customerServiceMockResponse);
+    });
+  }));
+
+  it('should return a single customer by id', waitForAsync(() => {
+    service
+      .getCustomer(customerServiceMockResponse[0].id)
+      .subscribe((response) => {
+        expect(response).toEqual(customerServiceMockResponse[0]);
+      });
+  }));
+
+  it('should return a filtered list of customers', waitForAsync(() => {
+    // Take a substring of the first customer's surname
+    const searchTerm = customerServiceMockResponse[0].surname.substring(0, 3);
+
+    service.searchCustomers(searchTerm).subscribe((response) => {
+      expect(response.customers).toContain(customerServiceMockResponse[0]);
+      expect(response.customers).not.toContain(customerServiceMockResponse[1]);
+    });
+  }));
+
+  it('handles gracefully when an empty search term is provided', waitForAsync(() => {
+    service.searchCustomers(null).subscribe((response) => {
+      expect(response.customers).toEqual(customerServiceMockResponse);
+    });
+  }));
 });
